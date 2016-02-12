54cc6d5dd8d81d726c6163b7f5bead3de3a7d772
Paul Sniezak <paulsniezak@gmail.com>
2016-02-12 01:13:33 +0100

    CSS-IMG-JS minifier/autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-var gulp       = require('gulp'),
-    handlebars = require('gulp-handlebars'),
-    wrap       = require('gulp-wrap'),
-    declare    = require('gulp-declare'),
-    watch      = require('gulp-watch'),
-    sass      = require('gulp-sass'),
-    concat     = require('gulp-concat');
+var gulp        = require('gulp'),
+    handlebars  = require('gulp-handlebars'),
+    wrap        = require('gulp-wrap'),
+    declare     = require('gulp-declare'),
+    watch       = require('gulp-watch'),
+    sass        = require('gulp-sass'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    postcss     = require('gulp-postcss'),
+    autop       = require('autoprefixer'),
+    cssnano     = require('gulp-cssnano'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant');
 
 
 var source = {
-    path: 'source/',
-    templates: 'source/templates/*.hbs',
-    scripts: 'source/scripts/*.js',
-    vendors: 'source/vendors/*.js',
-    sass:    'source/sass/*.scss'
+    path:       'source/',
+    templates:  'source/templates/*.hbs',
+    scripts:    'source/scripts/*.js',
+    vendors:    'source/vendors/*.js',
+    sass:       'source/sass/**/*.*',
+    images:      'source/images/*.{gif,jpg,png,svg}'
 };
 
 var dist = {
-    path: 'dist/',
-    scripts: 'dist/scripts/',
-    stylesheets: 'dist/stylesheets/'
-}
-
-// Listen changes /watch
-gulp.task('watch', function() {
-  gulp.watch( source.vendors, ['js_libs']);
-  gulp.watch( source.scripts, ['js_app']);
-  gulp.watch( source.sass, ['sass']);
-  gulp.watch( source.templates, ['bhs']);
-});
+    path:       'dist/',
+    scripts:    'dist/scripts/',
+    styles:     'dist/styles/',
+    images:     'dist/images/'
+};
 
 // Templates HBS
 gulp.task('hbs', function() {
@@ -36,9 +36,10 @@ gulp.task('hbs', function() {
         .pipe(wrap('Handlebars.template(<%= contents %>)'))
         .pipe(declare({
             namespace: 'templates',
-            noRedeclare: true,
+            noRedeclare: true
         }))
         .pipe(concat('templates.js'))
+        .pipe(uglify())
         .pipe(gulp.dest(dist.scripts));
 });
 
@@ -46,6 +47,7 @@ gulp.task('hbs', function() {
 gulp.task('js_app', function() {
     gulp.src(source.scripts)
         .pipe(concat("app.js"))
+        .pipe(uglify())
         .pipe(gulp.dest(dist.scripts));
 });
 
@@ -53,17 +55,40 @@ gulp.task('js_app', function() {
 gulp.task('js_libs', function() {
     gulp.src(source.vendors)
         .pipe(concat("vendors.js"))
+        .pipe(uglify())
         .pipe(gulp.dest(dist.scripts));
 });
 
 // SASS
-gulp.task('sass', function(){
+gulp.task('sass', function() {
   return gulp.src(source.sass)
-        .pipe(sass({
+      .pipe(sass({
           indentedSyntax: false
-        }))
-        .pipe(concat("style.css"))
-        .pipe(gulp.dest(dist.stylesheets))
+      }))
+      .pipe(concat("style.css"))
+      .pipe(postcss([ autop({ browsers: ['last 2 versions'] }) ]))
+      .pipe(cssnano())
+      .pipe(gulp.dest(dist.styles));
 });
 
-gulp.task('default', ['hbs', 'js_app', 'js_libs', 'sass','watch']);
+// Image minifier
+gulp.task('image_min', function() {
+    return gulp.src(source.images)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(dist.images));
+});
+
+// Build
+gulp.task('build', ['hbs', 'js_app', 'js_libs', 'sass', 'image_min']);
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch(source.vendors, ['js_libs']);
+    gulp.watch(source.scripts, ['js_app']);
+    gulp.watch(source.sass, ['sass']);
+    gulp.watch(source.templates, ['bhs']);
+});
